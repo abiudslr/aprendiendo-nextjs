4b4abb08129144c2ad937f533553a9cb8314d85b
abiudslr <ab-iud@hotmail.com>
2023-11-02 09:07:20 -0600

    posibles problemas con next 14 :(

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter as FontSans} from 'next/font/google'
 import {useLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme.provider';
+import { Nav } from '@/components/Nav';
 
-const inter = Inter({subsets: ['latin']})
+const inter = FontSans({subsets: ['latin'], variable: '--font-sans'})
  
 const locales = ['en', 'es'];
  
@@ -13,8 +16,21 @@ export default function LocaleLayout({children, params: {locale}} : {children:an
   if (!isValidLocale) notFound();
  
   return (
-    <html lang={locale}>
-      <body className={"bg-zinc-800"}>{children}</body>
+    <html lang={locale} suppressHydrationWarning>
+        <head />
+        <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <Nav />
+            </header>
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,13 @@
+import { ModeToggle } from '@/components/ModeToggle';
+import { Button } from '@/components/ui/button';
 import {useTranslations} from 'next-intl';
  
 export default function Index() {
   const t = useTranslations('Index');
 
-  return <h1 className='text-center text-cyan-50 text-9xl font-bold hover:font-normal'>{t('title')}</h1>;
+  return <div>
+    <h1 className='text-center text-cyan-950 dark:text-cyan-50 text-9xl font-bold hover:font-normal'>{t('title')}</h1>
+    <Button>{t('button')}</Button>
+    <ModeToggle />
+  </div>;
 }
